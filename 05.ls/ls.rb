53a87e51ff7d2f55999b83c0ee7dc719350ac6b6
Kawase-Kota <kottan00@gmail.com>
2021-05-04 23:10:07 +0900

    rubocopのパス（一箇所修正できませんでした）とコードの確認を実施しました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,52 +1,51 @@
+# frozen_string_literal: true
+
 require 'optparse'
 require 'etc'
 
-params = ARGV.getopts("a", "l", "r")
-lists = Dir.glob("*")
+params = ARGV.getopts('a', 'l', 'r')
+lists = Dir.glob('*')
 
 # ls -aの処理
-if params["a"] == true
-  lists = Dir.glob("*", File::FNM_DOTMATCH)
-end
+lists = Dir.glob('*', File::FNM_DOTMATCH) if params['a'] == true
 
 # ls -rの処理
-if params["r"] == true
-  lists = lists.reverse 
-end
+lists = lists.reverse if params['r'] == true
 
 # ls -lの処理
-if params["l"] == true
+if params['l'] == true
   # ファイルタイプの変換
   def file_type(type)
     {
-      "file" => "-",
-      "directory" => "d"
+      'file' => '-',
+      'directory' => 'd'
     }[type]
   end
 
   # パーミッションの変換
-  def file_mode(mode) 
+  def file_mode(mode)
     e = []
     mode.each do |m|
-      if m == "0"
-        e << "---"
-      elsif m == "1"
-        e << "--x"
-      elsif m == "2"
-        e << "-w-"
-      elsif m == "3"
-        e << "-wx"
-      elsif m == "4"
-        e << "r--"
-      elsif m == "5"
-        e << "r-x"
-      elsif m == "6"
-        e << "rw-"
-      elsif m == "7"
-        e << "rwx"
+      case m
+      when '0'
+        e << '---'
+      when '1'
+        e << '--x'
+      when '2'
+        e << '-w-'
+      when '3'
+        e << '-wx'
+      when '4'
+        e << 'r--'
+      when '5'
+        e << 'r-x'
+      when '6'
+        e << 'rw-'
+      when '7'
+        e << 'rwx'
       end
     end
-    mode = e.join
+    e.join
   end
 
   total = 0
@@ -64,7 +63,7 @@ if params["l"] == true
     file_uid  = Etc.getpwuid(file.uid).name # オーナー名
     file_gid  = Etc.getgrgid(file.gid).name # グループ名
     file_size = file.size # バイトサイズ
-    file_time = file.mtime.strftime("%m %d %R") # タイムスタンプ
+    file_time = file.mtime.strftime('%m %d %R') # タイムスタンプ
     file_name = list # ファイル名
     print "#{file_type}#{file_mode} #{file_link.to_s.rjust(2)} #{file_uid}  #{file_gid}  #{file_size.to_s.rjust(4)} #{file_time} #{file_name}\n"
   end
@@ -72,9 +71,10 @@ if params["l"] == true
 end
 
 # 各要素のサイズを3で割り切れるように揃える。不揃いであればnilを追加する。
-if lists.size % 3 == 2
+case lists.size % 3
+when 2
   lists.push(nil)
-elsif lists.size % 3 == 1
+when 1
   lists.push << nil << nil
 end
 
@@ -88,7 +88,7 @@ transpose_lists = transpose_lists.transpose.flatten
 # 横に最大3列の表示を維持させて、それぞれのファイル間に空白を入れる。
 transpose_lists.each_with_index do |list, i|
   i += 1
-  if i % 3 == 0
+  if (i % 3).zero?
     print list
     print "\n"
   else
