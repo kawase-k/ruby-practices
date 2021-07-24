55e2d755ad587737bf1816c0fcf63dd6896413f1
Kawase-Kota <kottan00@gmail.com>
2021-07-25 00:28:33 +0900

    レビューの修正を行いました

---
+++ 08.ls_object/short_formatter.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class ShortFormatter
+  PADDING = 24
+
+  def initialize(file_details)
+    @file_details = file_details
+  end
+
+  def print_result
+    transposed_file_details = transpose_file_details(@file_details)
+    transposed_file_details.each do |file_details|
+      file_details.each do |file_detail|
+        print file_detail.path.ljust(PADDING) if file_detail
+      end
+      puts
+    end
+  end
+
+  private
+
+  COLUMNLENGTH = 3
+
+  def transpose_file_details(paths)
+    sliced_file_details = paths.each_slice(COLUMNLENGTH).to_a
+    flattened_file_details = sliced_file_details.flat_map { |file_details| file_details.values_at(0...COLUMNLENGTH) }
+    flattened_file_details.each_slice(flattened_file_details.size / COLUMNLENGTH).to_a.transpose
+  end
+end
